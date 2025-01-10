b920ce082421a6f86216812075c74b2b42be1f78
kaita <johnkaitadocuments@gmail.com>
2025-01-11 00:54:02 +0300

    Working static files reference

--- mpesainvoices/models.py
+++ mpesainvoices/models.py
@@ -9,5 +9,8 @@ class MpesaInvoice(ParentModel):
     celebservice = models.ForeignKey(CelebService, default=None, null=True, on_delete=models.CASCADE, related_name="mpesainvoices")
     client = models.ForeignKey(AppUser, default=None, null=True, on_delete=models.CASCADE, related_name="mpesainvoices")
     invoice = models.CharField(max_length=255, default="None")
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
     def __str__(self):
         return f"Client {self.client.id} - {self.celebservice.serviceid.name}"
